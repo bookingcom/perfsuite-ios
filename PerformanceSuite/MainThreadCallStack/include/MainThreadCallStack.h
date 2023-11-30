c8a0a5d0d6467db2a3805c3439bba258f61566d8
Gleb Tarasov <gleb.tarasov@booking.com>
2023-11-30 19:55:39 +0100

    Fix compilation on Intel machines

--- PerformanceSuite/MainThreadCallStack/include/MainThreadCallStack.h
+++ PerformanceSuite/MainThreadCallStack/include/MainThreadCallStack.h
@@ -9,7 +9,7 @@ typedef struct {
 
 thread_state_result read_thread_state(mach_port_t thread);
 
+#endif
+
 // I couldn't call this function from swift, not sure why, that's why I'm re-exporting it here
 extern const char *macho_arch_name_for_mach_header_reexported(void) __API_AVAILABLE(ios(16.0));
-
-#endif

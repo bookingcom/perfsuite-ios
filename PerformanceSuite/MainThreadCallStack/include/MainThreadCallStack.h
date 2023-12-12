78155f9e6340e03536bef46d795609ec70fcf76f
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2023-12-12 14:23:22 +0000

    Update MainThreadCallStack.h

--- PerformanceSuite/MainThreadCallStack/include/MainThreadCallStack.h
+++ PerformanceSuite/MainThreadCallStack/include/MainThreadCallStack.h
@@ -11,5 +11,6 @@ thread_state_result read_thread_state(mach_port_t thread);
 
 #endif
 
+#include <Availability.h>
 // I couldn't call this function from swift, not sure why, that's why I'm re-exporting it here
 extern const char *macho_arch_name_for_mach_header_reexported(void) __API_AVAILABLE(ios(16.0));

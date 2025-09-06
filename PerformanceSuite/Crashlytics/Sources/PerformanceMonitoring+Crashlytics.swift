64f72b19421fe2075d6ef1b262aad4108548f478
Gleb Tarasov <gleb.tarasov@booking.com>
2025-09-06 11:56:21 +0200

    Added ability to disable crashlytics in DEBUG

--- PerformanceSuite/Crashlytics/Sources/PerformanceMonitoring+Crashlytics.swift
+++ PerformanceSuite/Crashlytics/Sources/PerformanceMonitoring+Crashlytics.swift
@@ -23,28 +23,40 @@ extension PerformanceMonitoring {
     ///   - hangsReportingMode: Defines which hangs and how we send to Crashlytics
     ///   - storage: Simple key/value storage which we use to store some simple objects, by default `UserDefaults` is used.
     ///   - experiments: Feature flags that can be used to enable/disable some experimentation features inside PerformanceSuite. Is used for A/B testing in production.
+    ///   - crashlyticsEnabledInDebug: If `false`, we won't report anything to Crashlytics if `DEBUG` is true.
     ///   **NB:** *Make sure that `FirebaseApp.configure() is called before calling this method! App will crash otherwise.*`
     public static func enableWithCrashlyticsSupport(
         config: Config = [],
         settings: CrashlyticsHangsSettings,
         storage: Storage = UserDefaults.standard,
-        experiments: Experiments = Experiments()
+        experiments: Experiments = Experiments(),
+        crashlyticsEnabledInDebug: Bool = true,
     ) throws {
-        guard FirebaseApp.allApps?.count ?? 0 > 0 else {
-            fatalError("Firebase is not configured yet. Please call `FirebaseApp.configure()` before calling this method.")
-        }
-
-        let didCrashPreviously = Crashlytics.crashlytics().didCrashDuringPreviousExecution()
-        let wrappedConfig = config.map { c in
-            switch c {
-            case .hangs(let receiver):
-                // wrap hang receiver with Crashlytics wrapper to send hangs to Crashlytics
-                return ConfigItem.hangs(CrashlyticsHangsReceiverWrapper(hangsReceiver: receiver, settings: settings))
-            default:
-                return c
+        #if DEBUG
+        let crashlyticsEnabled = crashlyticsEnabledInDebug
+        #else
+        let crashlyticsEnabled = true
+        #endif
+        
+        if crashlyticsEnabled {
+            guard FirebaseApp.allApps?.count ?? 0 > 0 else {
+                fatalError("Firebase is not configured yet. Please call `FirebaseApp.configure()` before calling this method.")
             }
+            
+            let didCrashPreviously = Crashlytics.crashlytics().didCrashDuringPreviousExecution()
+            let wrappedConfig = config.map { c in
+                switch c {
+                case .hangs(let receiver):
+                    // wrap hang receiver with Crashlytics wrapper to send hangs to Crashlytics
+                    return ConfigItem.hangs(CrashlyticsHangsReceiverWrapper(hangsReceiver: receiver, settings: settings))
+                default:
+                    return c
+                }
+            }
+            
+            try PerformanceMonitoring.enable(config: wrappedConfig, storage: storage, didCrashPreviously: didCrashPreviously, experiments: experiments)
+        } else {
+            try PerformanceMonitoring.enable(config: config, storage: storage, didCrashPreviously: false, experiments: experiments)
         }
-
-        try PerformanceMonitoring.enable(config: wrappedConfig, storage: storage, didCrashPreviously: didCrashPreviously, experiments: experiments)
     }
 }
--- PerformanceSuite/Tests/PerformanceMonitoringTests.swift
+++ PerformanceSuite/Tests/PerformanceMonitoringTests.swift
@@ -100,6 +100,19 @@ final class PerformanceMonitoringTests: XCTestCase {
         // cleanup PerformanceSuite
         try PerformanceMonitoring.disable()
     }
+    
+    func testEnableWithDisabledCrashlytics() async throws {
+        let settings = CrashlyticsHangsSettings()
+        // it shouldn't crash without crashlytics initialized
+        try PerformanceMonitoring.enableWithCrashlyticsSupport(config: .all(receiver: self), settings: settings, crashlyticsEnabledInDebug: false)
+        
+        let hangReporter = try XCTUnwrap(PerformanceMonitoring.appReporters.compactMap { $0 as? HangReporter }.first)
+        // check that hangsReceiver is not wrapped
+        XCTAssertTrue(hangReporter.receiver is PerformanceMonitoringTests)
+        
+        // cleanup PerformanceSuite
+        try PerformanceMonitoring.disable()
+    }
 
     private func customHangTypeFormatter(_ fatal: Bool, _ startup: Bool) -> String {
         return "hang_type"

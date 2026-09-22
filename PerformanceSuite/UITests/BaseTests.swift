453d9f18d3949048b99801103c187b0db8b4cadb
Gleb Tarasov <gleb.tarasov@booking.com>
2026-09-22 15:57:51 +0200

    Avoid racing UI test delay timers against expectation timeouts

--- PerformanceSuite/UITests/BaseTests.swift
+++ PerformanceSuite/UITests/BaseTests.swift
@@ -32,10 +32,10 @@ class BaseTests: XCTestCase {
 
     func waitForTimeout(_ seconds: Int) {
         let exp = expectation(description: "wait for timeout")
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(seconds)) {
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: Double(seconds + 1))
+        // Let XCTest wait out the interval while processing events. A separate
+        // dispatch timer can run late on CI and race the expectation's timeout.
+        exp.isInverted = true
+        wait(for: [exp], timeout: Double(seconds))
     }
 
     func assertNoMessages(file: StaticString = #file, line: UInt = #line, _ messages: Message...) {
